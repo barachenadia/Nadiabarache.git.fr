6046aab5e75e92e12c33e83cae20678470f51dfd
barachenadia <barache.nadia@yahoo.fr>
2015-07-04 17:11:53 +0200

    Add tag

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -24,9 +24,9 @@ class DefaultController extends Controller
         $tags2 =array('Smfony2', 'Doctrine', 'Twig', 'Foundation', 'GIT', 'Capifony' );
         $tags3 =array('Cordova', 'JQuery', 'mobile', 'android', 'IOS');
         $tags4 =array('Ruby', 'On', 'Rails','Bootstrap', 'GIT', 'Heroku' );
-        return $this->render('default/porfolio.html.twig',array('tags1' => $tags1
-                                                                'tags2' => $tags2
-                                                                 'tags3' => $tags3
-                                                                 'tags4' => $tags4 ));
+        return $this->render('default/porfolio.html.twig', array('tags1' => $tags1,
+                                                                'tags2' => $tags2,
+                                                                'tags3' => $tags3,
+                                                                'tags4' => $tags4, ));
     }
 }
