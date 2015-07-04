c935f49a939509bd93ffa9016e2007b56974b776
barachenadia <barache.nadia@yahoo.fr>
2015-07-04 17:07:19 +0200

    Add porfolio tag

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -20,7 +20,13 @@ class DefaultController extends Controller
      */
      public function porfolioAction()
     {
-
-        return $this->render('default/porfolio.html.twig');
+        $tags1 =array('Smfony2', 'Doctrine', 'Twig', 'Bundle', 'GIT', 'Bootstrap' );
+        $tags2 =array('Smfony2', 'Doctrine', 'Twig', 'Foundation', 'GIT', 'Capifony' );
+        $tags3 =array('Cordova', 'JQuery', 'mobile', 'android', 'IOS');
+        $tags4 =array('Ruby', 'On', 'Rails','Bootstrap', 'GIT', 'Heroku' );
+        return $this->render('default/porfolio.html.twig',array('tags1' => $tags1
+                                                                'tags2' => $tags2
+                                                                 'tags3' => $tags3
+                                                                 'tags4' => $tags4 ));
     }
 }
